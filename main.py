827efb42b2a5115fd49f95665a316f3e852e51d4
YongGyunKwon <ygkwon50@gmail.com>
2020-04-24 00:38:52 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame.mixer
+import time
+from PIL import Image
+from pytesseract import *
+from gtts import gTTS
+from datetime import datetime
+
+
+
+
+
+if __name__ =="__main__":
+        print("hello")
