8bac4cac4b9d6de432607cb60b4dc76e58ddb48c
DG <ehdrb1205@naver.com>
2020-06-15 17:39:28 +0900

    modify UI(재수정)

--- UI/MainScreen.py
+++ UI/MainScreen.py
@@ -1,10 +1,8 @@
 import sys
 from PyQt5.QtWidgets import *
 import MainUI
-from PyQt5.QtGui import *
-from PyQt5 import QtCore, QtGui, QtWidgets
-#from Fuction.function import *
-
+from Fuction.function import *
+from Fuction.Image_Converter  import *
 """
 A. 변환후 저장 경로가 반환된다면?
 1) 사운드 파일의 이름은 사용자 마음대로 설정할 수 있음(추후 시간있을 때 구현)
@@ -42,6 +40,7 @@ class WindowClass(QMainWindow, MainUI.Ui_Dialog) :
         self.TransFileBtn.clicked.connect(self.Push_TransFileButton)
         self.DeleteFileBtn.clicked.connect(self.Push_DeleteButton)
         self.Save_path_setting.clicked.connect(self.SavePath)
+        self.PreviewButton.clicked.connect(self.Preview)
         self.HowToBtn.clicked.connect(self.HowTo)
 
     ##파일 추가 버튼 동작 메서드## #완료
@@ -62,7 +61,6 @@ class WindowClass(QMainWindow, MainUI.Ui_Dialog) :
             self.num = 17
             self.File_Path += QFileDialog.getOpenFileName(self, 'Open file', '/', "PDF Files (*.pdf)")
             if self.File_Path[-1] == '':
-                del self.File_Path[-1]
                 del self.File_Path[-1]
                 return 0
             self.PDF_name = ''.join(self.File_Path).split('/')[-1]
@@ -96,7 +94,6 @@ class WindowClass(QMainWindow, MainUI.Ui_Dialog) :
         return 1
 
     ##파일 변환 버튼 동작 메서드##
-
     def Push_TransFileButton(self):
         self.SoundType = self.SoundTypeBox.currentText()    # 파일형식선택
         if self.SoundType == "File Format":
@@ -104,19 +101,66 @@ class WindowClass(QMainWindow, MainUI.Ui_Dialog) :
         if self.Sound_Path == "":
             self.warning_3()
 
+            ###기능 구현 동작과 연결
+            if(self.File_Path[1]=='PDF Files (*.pdf)'):
+
+                print(self.File_Path[0])
+                transtext = read_pdf_file1(self.File_Path[0])
+                print(transtext)
+                transpath = self.Sound_Path + "/" + self.PDF_name[:self.strlen - 5]
+                print(transpath)
+
+                g_tts(transtext, transpath, self.SoundType)
+
+
+            elif(self.File_Path[1]=='JPG Files (*.jpg)'):
+                print("This is jpg")
+                print(self.File_Path[0])
+                transtext1=image_to_string1(self.File_Path[0])
+                print(transtext1)
+                transpath1 = self.Sound_Path + "/" + self.PDF_name[:self.strlen - 4]
+                print(transpath1)
+
+                g_tts(transtext1, transpath1, self.SoundType)
+
+
+            elif (self.File_Path[1] == 'PNG Files (*.png)'):
+                print("This is png")
+
+                print(self.File_Path[0])
+                transtext1 = image_to_string1(self.File_Path[0])
+                print(transtext1)
+                transpath1 = self.Sound_Path + "/" + self.PDF_name[:self.strlen - 4]
+                print(transpath1)
+
+                g_tts(transtext1, transpath1, self.SoundType)
+
+            #docx 완료
+            elif(self.File_Path[1]=="DOCX Files (*.docx)"):
+
+                print(self.File_Path[0])
+                transtext=read_docx_file(self.File_Path[0])
+                print(transtext)
+                transpath=self.Sound_Path+"/"+self.PDF_name[:self.strlen - 5]
+                print(transpath)
+
+                g_tts(transtext,transpath,self.SoundType)
+
+            self.ShowFile()     # 기능 구현 코드(파일 변환 코드)에서 변환이 완료되면 변환된 파일 표시
+
+
     ##불러온 파일 삭제 동작 메서드## #완료
     def Push_DeleteButton(self):
         if self.File_Path == []:
             return
         item = self.PDF_FileList.currentItem()
-        if(item == None):
+        if (item == None):
             return
-        N = 2 * self.PDF_FileList.currentRow() #선택된 항목이 몇 번째인지 반환
-        self.PDF_FileList.takeItem(self.PDF_FileList.row(item)) #ListWidget에서 삭제
-        del self.File_Path[N] #리스트에서 파일 삭제
-        del self.File_Path[N] #리스트에서 파일의 확장자 삭제
+        N = 2 * self.PDF_FileList.currentRow()  # 선택된 항목이 몇 번째인지 반환
+        self.PDF_FileList.takeItem(self.PDF_FileList.row(item))  # ListWidget에서 삭제
+        del self.File_Path[N]  # 리스트에서 파일 삭제
+        del self.File_Path[N]  # 리스트에서 파일의 확장자 삭제
         ###기능 구현 동작과 연결
-
 
     def SavePath(self):
      self.Sound_Path = QFileDialog.getExistingDirectory(self, 'Open Folder', 'c:/')     # Sound_Path에 저장할 경로 저장
@@ -130,7 +174,11 @@ class WindowClass(QMainWindow, MainUI.Ui_Dialog) :
         self.Sound_FileList.addItem(self.Sound_Name)
         print(self.Sound_FileList.currentItem())
 
-    ##경고문 띄우기
+    def Preview(self):
+        files=self.File_Path[0]
+        Image_Converter(files)
+
+ ##경고문 띄우기
     def warning_1(self):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Warning)
@@ -164,6 +212,7 @@ class WindowClass(QMainWindow, MainUI.Ui_Dialog) :
         """)
         msg.setWindowTitle("HowTo")
         msg.exec_()
+
 
 if __name__ == "__main__" :
     #QApplication : 프로그램을 실행시켜주는 클래스
